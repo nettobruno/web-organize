862d5290fc47bdbf228c6355a5b97ebc1126275b
Bruno Netto <brunonettomac@outlook.com>
2021-04-27 20:49:33 -0300

    Adding all the content of the welcome screen

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -4,6 +4,10 @@ import React from 'react';
 // Images
 import firstImage from '../../assets/first.svg';
 import secondImage from '../../assets/second.svg';
+import thirdImage from '../../assets/third.svg';
+import fourthImage from '../../assets/fourth.svg';
+import fifthImage from '../../assets/fifth.svg';
+import sixthImage from '../../assets/sixth.svg';
 
 // Styles
 import { Container, Section, Content, BlockTexts, BlockImage } from './styles';
@@ -44,6 +48,84 @@ function Welcome() {
                 dia sai alguma novidade.
               </p>
               <p>Precisamos ficar sempre atentos para não perder nada.</p>
+            </BlockTexts>
+          </Content>
+        </div>
+      </Section>
+
+      <Section className="bg-rose">
+        <div className="container">
+          <Content>
+            <BlockTexts>
+              <h2>Foco e progresso</h2>
+              <p>
+                Para que a gente consiga alcançar nos nossos objetivos é preciso
+                ter muito foco, e principalmente organização para não deixar
+                nada passar em branco.
+              </p>
+            </BlockTexts>
+
+            <BlockImage>
+              <img src={thirdImage} alt="" />
+            </BlockImage>
+          </Content>
+        </div>
+      </Section>
+
+      <Section className="bg-yellow">
+        <div className="container">
+          <Content>
+            <BlockImage>
+              <img src={fourthImage} alt="" />
+            </BlockImage>
+
+            <BlockTexts>
+              <h2>Não esqueça de nada</h2>
+              <p>
+                Com tantas coisas para fazer a falta de organização bate na
+                porta, e a gente pode esquecer de fazer coisa simples mas
+                extremamente importantes, como praticar atividades físicas
+              </p>
+            </BlockTexts>
+          </Content>
+        </div>
+      </Section>
+
+      <Section className="bg-blue">
+        <div className="container">
+          <Content>
+            <BlockTexts>
+              <h2>Ganhe produtividade</h2>
+              <p>
+                O trabalho já não vai render tanto como antes, as tarefas irão
+                acumular, o estresse aumenta, e a saúde mental vai pra longe
+              </p>
+
+              <p>Não queremos que isso aconteça</p>
+            </BlockTexts>
+
+            <BlockImage>
+              <img src={fifthImage} alt="" />
+            </BlockImage>
+          </Content>
+        </div>
+      </Section>
+
+      <Section className="bg-rose">
+        <div className="container">
+          <Content>
+            <BlockImage>
+              <img src={sixthImage} alt="" />
+            </BlockImage>
+
+            <BlockTexts>
+              <h3>Bora Anotar</h3>
+              <p>
+                Nós vamo te ajudar a manter a produtividade e organização em
+                alta. Faça seu login, e começe a mudança no seu dia a dia
+              </p>
+
+              <a href="/login">Acessar</a>
             </BlockTexts>
           </Content>
         </div>
--- src/pages/Welcome/styles.js
+++ src/pages/Welcome/styles.js
@@ -35,15 +35,22 @@ export const BlockTexts = styled.div`
     font-size: 7em;
   }
 
-  div {
-    margin-top: 35px;
-  }
-
   h2 {
     color: white;
     font-family: 'Ubuntu', sans-serif;
     font-size: 3em;
     margin-bottom: 20px;
+  }
+
+  h3 {
+    color: white;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 6em;
+    margin-bottom: 20px;
+  }
+
+  div {
+    margin-top: 35px;
   }
 
   p {
@@ -53,6 +60,32 @@ export const BlockTexts = styled.div`
     margin-bottom: 5px;
     line-height: 30px;
   }
+
+  a {
+    background-color: rgba(83, 82, 237, 0.8);
+    border-radius: 10px;
+    color: white;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.4em;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    width: 30%;
+    margin: 20px 0 0;
+    padding: 15px 0;
+    transition: 0.3s;
+  }
+
+  a:hover {
+    background-color: rgba(83, 82, 237, 0.6);
+  }
 `;
 
-export const BlockImage = styled.div``;
+export const BlockImage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100vh;
+`;
