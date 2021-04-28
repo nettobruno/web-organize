b4b823a7b98818694bf2712635dddbaec7007c7f
Bruno Netto <brunonettomac@outlook.com>
2021-04-27 22:03:46 -0300

    Adding register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,19 +1,22 @@
 // Dependencies
 import React, { useRef, useState } from 'react';
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import api from '../../services/api';
 
 // Components
 import SimpleInput from '../../components/SimpleInput';
-// import { showToast } from '../../components/Alert';
+import { showToast } from '../../components/Alert';
 import Loader from '../../components/Loader';
 
+// Images
+import registerImage from '../../assets/register.svg';
+
 // styles
-import { Container, FormContent } from './styles';
+import { Container, BlockForm, BlockImage, FormContent } from './styles';
 
 function Register() {
   const formRef = useRef();
-  // const history = useHistory();
+  const history = useHistory();
   const [loader, setLoader] = useState(false);
 
   async function handleSubmit(data) {
@@ -26,15 +29,36 @@ function Register() {
           password: data.password,
         })
         .then((response) => {
-          console.log(response);
+          if (response.data.error) {
+            showToast({
+              type: 'error',
+              message: response.data.error,
+            });
+          }
+
+          if (response.data) {
+            showToast({
+              type: 'success',
+              message: response.data.message,
+            });
+
+            history.push('/login');
+          }
           setLoader(false);
         })
-        .catch((err) => {
-          console.log(err);
+        .catch((error) => {
+          showToast({
+            type: 'error',
+            message: error.response.data.error,
+          });
           setLoader(false);
         });
     } catch (err) {
       console.log(err);
+      showToast({
+        type: 'error',
+        message: 'Algum erro interno aconteceu. Tente novamente mais tarde',
+      });
       setLoader(false);
     }
   }
@@ -42,13 +66,20 @@ function Register() {
   return (
     <Container>
       <Loader loader={loader} />
-      <h2>Registro</h2>
-      <FormContent ref={formRef} onSubmit={handleSubmit}>
-        <SimpleInput name="name" placeholder="Nome" type="text" />
-        <SimpleInput name="email" placeholder="E-mail" type="email" />
-        <SimpleInput name="password" placeholder="Senha" type="password" />
-        <button type="submit">Acessar</button>
-      </FormContent>
+      <BlockForm>
+        <h2>Cadastrar</h2>
+        <FormContent ref={formRef} onSubmit={handleSubmit}>
+          <SimpleInput name="name" placeholder="Nome" type="text" />
+          <SimpleInput name="email" placeholder="E-mail" type="email" />
+          <SimpleInput name="password" placeholder="Senha" type="password" />
+          <button type="submit">Cadastrar</button>
+          <a href="/login">Já tem uma conta? Entrar</a>
+        </FormContent>
+      </BlockForm>
+
+      <BlockImage>
+        <img src={registerImage} alt="" />
+      </BlockImage>
     </Container>
   );
 }
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -2,9 +2,69 @@ import styled from 'styled-components';
 import { Form } from '@unform/web';
 
 export const Container = styled.div`
-  border: 1px solid red;
+  background-color: rgba(83, 82, 237, 0.6);
+
+  height: 100vh;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+export const BlockForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h2 {
+    color: white;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 4em;
+    margin-bottom: 20px;
+  }
+
+  button {
+    background-color: #5352ed;
+    border-radius: 5px;
+    color: white;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.4em;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    width: 100%;
+    margin: 20px 0 0;
+    padding: 15px 0;
+    transition: 0.3s;
+  }
+
+  button:hover {
+    background-color: #6f6ef8;
+  }
+
+  a {
+    color: rgba(255, 255, 255, 0.5);
+    margin-top: 30px;
+  }
+
+  a:hover {
+    color: rgba(255, 255, 255);
+  }
+`;
+
+export const BlockImage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const FormContent = styled(Form)`
-  border: 1px solid blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  width: 400px;
 `;
