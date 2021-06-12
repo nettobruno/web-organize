413ad33cb18a685831d6a7544bed2d36f1119974
Bruno Netto <brunonettomac@outlook.com>
2021-06-12 03:23:23 -0300

    Adding 404 Screen

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -1,16 +1,40 @@
 // Dependencies
 import React from 'react';
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+
+// Imagens
+import image404 from '../../assets/404.svg';
 
 // styles
-import { Container } from './styles';
+import { Container, Content } from './styles';
 
 function Page404() {
-  // const history = useHistory();
+  const history = useHistory();
+
+  function back() {
+    history.goBack();
+  }
 
   return (
-    <Container className="container">
-      <h1>404</h1>
+    <Container>
+      <div className="container">
+        <Content>
+          <div className="texts">
+            <h1>Página não encontrada</h1>
+            <p>
+              Opa, não esperava ver você por aqui. Infelizmente essa página não
+              existe, clique no botão abaixo e volte para o início.
+            </p>
+            <div className="btn">
+              <button type="button" onClick={() => back()}>
+                Voltar
+              </button>
+            </div>
+          </div>
+
+          <img src={image404} alt="" />
+        </Content>
+      </div>
     </Container>
   );
 }
--- src/pages/Page404/styles.js
+++ src/pages/Page404/styles.js
@@ -1,5 +1,69 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  border: 1px solid red;
+  background-color: rgba(55, 66, 250, 0.6);
+
+  height: 100vh;
+  width: 100%;
+
+  padding-top: 100px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 300px;
+
+  h1 {
+    color: white;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 4em;
+    margin-bottom: 20px;
+    text-align: center;
+  }
+
+  p {
+    font-size: 1.6em;
+    color: white;
+    font-family: 'Ubuntu', sans-serif;
+
+    width: 70%;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  button {
+    background-color: #5352ed;
+    border-radius: 5px;
+    color: white;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.4em;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    margin: 20px 0;
+    padding: 15px 50px;
+    transition: 0.3s;
+  }
+
+  button:hover {
+    background-color: #6f6ef8;
+  }
+
+  img {
+    position: absolute;
+    bottom: 0;
+  }
 `;
