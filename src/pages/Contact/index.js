bf29b5cecf963d592247601e044e222a2afa289c
Bruno Netto <brunonettomac@outlook.com>
2021-06-12 04:04:19 -0300

    Adding Contact screen

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,13 +1,82 @@
 // Dependencies
-import React from 'react';
+import React, { useRef } from 'react';
 
-// Styles
-import { Container } from './styles';
+// Components
+import SimpleInput from '../../components/SimpleInput';
+
+// Imagens
+import imageGithub from '../../assets/github.svg';
+
+// styles
+import {
+  Container,
+  Content,
+  ThreeColumns,
+  CircleGithub,
+  FormContent,
+} from './styles';
 
 function Contact() {
+  const formRef = useRef();
+
+  async function handleSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <Container>
-      <h1>CONTACT</h1>
+      <div className="container">
+        <Content>
+          <h1>Gostou do Projeto?</h1>
+          <ThreeColumns>
+            <h2>É open source</h2>
+            <div className="inline">
+              <div className="line" />
+              <h3> ou </h3>
+              <div className="line" />
+            </div>
+            <h2>Entre em contato</h2>
+          </ThreeColumns>
+
+          <ThreeColumns>
+            <div className="link">
+              <CircleGithub
+                href="https://github.com/nettobruno/web-organize"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={imageGithub} alt="" />
+              </CircleGithub>
+              <p>
+                Feito com ❤️ por{' '}
+                <a
+                  href="https://brunonetto.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Bruno Netto
+                </a>
+              </p>
+            </div>
+
+            <div />
+
+            <div className="form">
+              <FormContent ref={formRef} onSubmit={handleSubmit}>
+                <SimpleInput name="name" placeholder="Nome" type="text" />
+                <SimpleInput name="email" placeholder="E-mail" type="email" />
+                <SimpleInput
+                  multiline
+                  name="message"
+                  placeholder="Mensagem"
+                  type="text"
+                />
+                <button type="submit">Enviar</button>
+              </FormContent>
+            </div>
+          </ThreeColumns>
+        </Content>
+      </div>
     </Container>
   );
 }
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -2,9 +2,122 @@ import styled from 'styled-components';
 import { Form } from '@unform/web';
 
 export const Container = styled.div`
-  border: 1px solid red;
+  background-color: rgba(55, 66, 250, 0.6);
+
+  height: 100vh;
+  width: 100%;
+
+  padding-top: 100px;
+`;
+
+export const Content = styled.div`
+  h1 {
+    color: white;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 4em;
+    margin-bottom: 100px;
+    text-align: center;
+  }
+`;
+
+export const ThreeColumns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+  width: 100%;
+
+  h2 {
+    color: white;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 2em;
+  }
+
+  div.inline {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div.inline .line {
+    width: 70px;
+    height: 4px;
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+
+  h3 {
+    color: rgba(255, 255, 255, 0.5);
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 1.6em;
+    margin: 0 15px;
+  }
+
+  p,
+  a {
+    color: white;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.4em;
+    font-weight: 400;
+    margin-top: 20px;
+  }
+
+  a {
+    font-size: 1em;
+  }
+
+  a:hover {
+    color: #6f6ef8;
+  }
+
+  .link,
+  .form {
+    margin: 80px auto 0;
+  }
+`;
+
+export const CircleGithub = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin: 0 auto;
+
+  background: rgba(255, 255, 255, 0.5);
+  border-radius: 100px;
+  width: 200px;
+  height: 200px;
+  transition: 0.3s;
+
+  :hover {
+    transform: scale(1.1);
+  }
 `;
 
 export const FormContent = styled(Form)`
-  border: 1px solid blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin: 0 auto;
+
+  width: 400px;
+
+  button {
+    background-color: #5352ed;
+    border-radius: 5px;
+    color: white;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.4em;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    width: 100%;
+    margin: 10px 0 0;
+    padding: 10px 0;
+    transition: 0.3s;
+  }
+
+  button:hover {
+    background-color: #6f6ef8;
+  }
 `;
